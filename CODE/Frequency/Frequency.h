f267fa9b7c3f458a77ed468a58a74a83a17f540d
Luda-Seliv-KE-413 <Luda_seliv@inbox.ru>
2024-05-04 18:51:59 +0500

    Внесение правок

--- CODE/Frequency/Frequency.h
+++ CODE/Frequency/Frequency.h
@@ -4,13 +4,12 @@
 #include "ICalculate.h" //for Calculate
 #include <iostream> // for std::cout
 
-//template
 class Frequency : public ICalculate //for Temperature converter Glycemia?
 {
 public:
   void Calculate() const override
   {
-    std::cout << "55 Hz" << std::endl; //необходимо дописать (данная строка для теста)
+    std::cout << "55 Hz" << std::endl; //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ (пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ)
   }
 };
 
--- CODE/main.cpp
+++ CODE/main.cpp
@@ -8,36 +8,14 @@
 #include "Temperature.h" // for Temperature
 #include "Glycemia.h" // for Glycemia
 #include "Frequency.h" // for Frequency
-//#include "BluetoothTask.h" // for BluetoothTask
-//#include "BatteryTask.h" // for BatteryTask
+//TODO
 
 std::uint32_t SystemCoreClock = 16'000'000U;
 
 extern "C" {
 int __low_level_init(void)
 {
-  /*//Usart
-  RCC::APB1ENR::USART2EN::Enable::Set(); // тактирование
-  GPIOA::MODER::MODER2::Alternate::Set(); // конфигурация портов, альтернативная функция
-  GPIOA::MODER::MODER3::Alternate::Set(); // конфигурация портов, альтернативная функция
-  GPIOA::OTYPER::OT2::OutputPushPull::Set();
-  GPIOA::OTYPER::OT3::OutputPushPull::Set();
-  GPIOA::PUPDR::PUPDR2::PullUp::Set(); // подтяжка к 1  
-  GPIOA::PUPDR::PUPDR3::PullUp::Set(); // подтяжка к 1 
-  
-  GPIOA::AFRL::AFRL2::Af7::Set();
-  GPIOA::AFRL::AFRL3::Af7::Set();
-  
-  USART2::BRR::DIV_Mantissa::Set(104);
-  USART2::BRR::DIV_Fraction::Value2::Set();
-  USART2::CR2::STOP::Value0::Set();
-    
-  USART2::CR1::OVER8::OversamplingBy16::Set(); // Режим дискретизации? 1/16
-  USART2::CR1::M::Data8bits::Set();
-  
-  USART2::CR1::TE::Enable::Set();
-  USART2::CR1::UE::Enable::Set();*/
-  
+  //TODO
   return 1;
 }
 }
